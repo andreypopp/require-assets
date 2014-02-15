960bd313f29aa40441d03c834ffb73f65937b0ae
Andrey Popp <8mayday@gmail.com>
2014-02-15 20:07:26 +0400

    Use require-assets-css for the example

--- example/main.js
+++ example/main.js
@@ -1,12 +1,2 @@
 var requireAssets = require('../index');
-
-var image = requireAssets('./image.png');
-console.log(image);
-
-if (typeof window !== 'undefined') {
-  window.onload = function() {
-    var img = document.createElement('img');
-    img.src = image;
-    document.body.appendChild(img);
-  }
-}
+var style = requireAssets('./main.css');
