008975612ff4feb6256f9396dff78ab663a58ee3
Andrey Popp <8mayday@gmail.com>
2014-02-17 01:44:03 +0400

    More orbust CSS handler

--- handlers/css.js
+++ handlers/css.js
@@ -10,8 +10,18 @@ function handleCSS(filename, url, registry) {
 
   asset.src = fs.readFileSync(filename, 'utf8').replace(
     /([^a-zA-Z0-9])url\(([^\)]+)\)/g, function(_m, prefix, ref) {
+      ref = ref
+        .replace(/^'/, '').replace(/'$/, '')
+        .replace(/^"/, '').replace(/"$/, '');
+
+      var suffix = '';
+      ref = ref.replace(/[\?#].+$/, function(m) {
+        suffix = m;
+        return '';
+      });
+
       var result = registry.requireAssets(ref, basedir);
-      return prefix + 'url(' + result + ')';
+      return prefix + 'url(' + result + suffix + ')';
     });
 
   return asset;
